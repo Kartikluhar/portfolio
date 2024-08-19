1aa0d70ad19e22effd2f7988147ca3eecf561d9a
Kartikluhar <137763617+Kartikluhar@users.noreply.github.com>
2024-08-19 11:26:58 +0530

    done

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ let navbar = document.getElementById("navbar");
 let nav_canvas = document.getElementById('nav-canvas')
 let logo = document.getElementById('logo')
 let loader = document.getElementById('loader')
+let darkModeBtn = document.getElementById("dark-mode-btn")
+let lightModeBtn = document.getElementById("light-mode-btn")
 
 let viewportWidth = window.innerWidth || document.documentElement.clientWidth;
 window.addEventListener("scroll",function(){
@@ -18,12 +20,10 @@ function toggleMenu(){
     if (flag == 0) {
         menu.style.top = '80px';
         nav_canvas.style.top = '0';
-        logo.style.opacity = '0';
         flag = 1
     } else {
         menu.style.top = '-30rem';
         nav_canvas.style.top = '-100%';
-        logo.style.opacity = '1';
         flag = 0
     }
 }
@@ -32,7 +32,6 @@ function menuLinkClicked(){
     if (flag == 1) {
         menu.style.top = '-30rem';
         nav_canvas.style.top = '-100%';
-        logo.style.opacity = '1';
         flag = 0
     }else{
         menu.style.top = '80px';
@@ -55,6 +54,37 @@ const sr = ScrollReveal({
     delay: 200,
     // reset: true
 });
-sr.reveal('.home',{delay: 4500})
-sr.reveal('.skills-paragraph, .footer-contacts, .about',{})
+sr.reveal('.home, .about',{delay: 4500})
+sr.reveal('.skills-paragraph, .footer-contacts',{})
 sr.reveal('.form-input, .skills-bar, .work-container a',{interval:200})
+
+
+// ========= Dark Light Mode ===========
+
+let darklightMode = 0;
+
+darkModeBtn.addEventListener("click",() =>{
+    darkModeBtn.style.display = 'none';
+    lightModeBtn.style.display = 'block';
+    document.documentElement.style.setProperty('--white', 'black'); // Example value, change as needed
+    document.documentElement.style.setProperty('--dark-black', 'white'); // Example value, change as needed
+    document.documentElement.style.setProperty('--light-black', 'rgb(240, 240, 240)'); // Example value, change as needed
+    document.documentElement.style.setProperty('--gray', 'rgb(30,30,30)'); // Example value, change as needed
+    document.documentElement.style.setProperty('--wheat', 'rgb(222, 156, 32)');
+    document.documentElement.style.setProperty('--light-white', 'rgb(70, 70, 70)');
+    document.documentElement.style.setProperty('--btn-gray', 'white');
+    document.documentElement.style.setProperty('--para-gray', 'rgb(60, 60 , 60)');
+})
+
+lightModeBtn.addEventListener("click",() => {
+    lightModeBtn.style.display = 'none';
+    darkModeBtn.style.display = 'block';
+    document.documentElement.style.setProperty('--white', 'white');
+    document.documentElement.style.setProperty('--dark-black', 'black');
+    document.documentElement.style.setProperty('--light-black', 'rgb(15, 15, 15)');
+    document.documentElement.style.setProperty('--gray', 'gray');
+    document.documentElement.style.setProperty('--wheat', 'wheat');
+    document.documentElement.style.setProperty('--light-white', 'rgb(223, 223, 223)');
+    document.documentElement.style.setProperty('--btn-gray', 'gray');
+    document.documentElement.style.setProperty('--para-gray', 'rgb(175,175,175)');
+})
